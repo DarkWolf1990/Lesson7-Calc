fdd5cdcedb7818efbb2a1c3c0a2bdbc6b14ec738
Skycraft <mip1991@mail.ru>
2022-07-29 19:24:14 +0300

    Complete iface

---
+++ Controller.py
@@ -0,0 +1,21 @@
+import Interface as Iface
+import Inpt as inp
+import Calculator as cl
+Iface.Intface("Welcome")
+while(True):
+    Iface.Intface("Menu")
+    i = int(inp.Variable())
+    match i:
+        case 1: 
+            print(cl.Calc(inp.Reader("Ir")))
+            Iface.Intface("End")
+            if(int(inp.Variable()) == 2): break
+        case 2: 
+            print(cl.Calc(inp.Reader("Comp")))
+            Iface.Intface("End")
+            if(int(inp.Variable()) == 2): break
+        case 3: print('Будет разработано когданибудь')
+        case 4: 
+            print('Запущен выход из программы')
+            break
+        case _: print("Нет данных")
