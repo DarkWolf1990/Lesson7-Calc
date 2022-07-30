c6722b1adf5ab46d5623ce6dfab2154cba63ee70
DarkWolf_ <whitejaguar08@gmail.com>
2022-07-30 21:23:55 +0300

    Update

--- guiInterface.py
+++ guiInterface.py
@@ -1,3 +1,3 @@
 # guiInterface.py
 import PySimpleGUI as sg
-sg.Window(title='Hi User', layout = [[]], margins = (300, 50)).read()
+sg.Window(title='Hi User', layout = [[]], margins = (250, 250)).read()
