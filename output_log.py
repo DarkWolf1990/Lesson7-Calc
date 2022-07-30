35e9d0a9067b010314a71bae02eb1619f6129769
DarkWolf_ <whitejaguar08@gmail.com>
2022-07-30 21:21:23 +0300

    Create GUI interface

--- output_log.py
+++ output_log.py
@@ -1,6 +1,8 @@
 import logging
 import logging.handlers
-import sys, socket
+import test
+from logger import handler 
+
 
 # шкала уровеней по приоритетам
 '''
@@ -13,4 +15,15 @@ ERROR - 40
 CRITICAL - 50
 
 ''' 
+
+file_log = logging.FileHandler('Log.txt')
+console_out = logging.StreamHandler()
+
+logging.basicConfig(handlers=(file_log, console_out), 
+                    format='[%(asctime)s | %(levelname)s]: %(message)s', 
+                    datefmt='%m.%d.%Y %H:%M:%S',
+                    level=logging.DEBUG)
+
+logging.debug('Info message??))')
+
 
