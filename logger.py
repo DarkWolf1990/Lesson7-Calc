85fc20bfd422d995845c6c326dff5215322f473c
DarkWolf_ <whitejaguar08@gmail.com>
2022-07-30 17:36:22 +0300

    update

--- logger.py
+++ logger.py
@@ -1,19 +1,32 @@
 # подключаем стандартную функцию логирования
 import logging
-import Calculator
-import Interface
+from Interface import Intface
+from Inpt import Reader
 # задаем базовую конфигурацию и выводим логи в файл(файлик у нас будет
 # перезаписываться внося новые данные), также
 # добавляем сразу дату и время,
-logging.basicConfig(level=logging.INFO, filename="log.txt", filemode ="w",
+logging.basicConfig(level=logging.DEBUG, filename="log.txt", filemode ="w",
                     format ="%(asctime)s -%(levelname)s - %(message)s")
-# это уровни журналаб выше в коде также сделана настройка журналов
+# получаем логи 
+logger = logging.getLogger(__name__)
 
-# Использую канкатинацию, чтобы склеить строки, 
-# также передаю в лог подключенные файлы.
-logging.info(f"{Calculator}")
-logging.debug("info")
-logging.warning("warning")
-logging.error("error")
-logging.critical(f"{Interface}")
+# настраиваю обработчик, чтобы журнал передавался в файл
+handler = logging.FileHandler('test.txt')
+# тут форматирую и передаю аргументы 
+formatter= logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+# вот здесь добавляю обработчик 
+logger.addHandler(handler)
+#подключаю функцию
+logger.debug('^)^')
+logger.debug(Reader)
+logger.debug(Intface)
+
+
+
+
+
+
+    
+   
 
