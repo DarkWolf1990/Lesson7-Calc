83d68464a7f81d073c98e1fa462166b9ba6765b1
Skycraft <mip1991@mail.ru>
2022-08-01 00:31:23 +0300

    Correct logger&output module

--- Main.Controller.py
+++ Main.Controller.py
@@ -13,7 +13,7 @@ while(True):
     i = int(inp.Variable())
     match i:
         case 1: 
-            ou.plog("Mode",i)
+            ou.plog("Mode",str(i))
             vrb = inp.Reader("Ir")
             ou.plog("Variables",vrb[0])
             ou.plog("Result",str(cl.Calc(vrb)))
@@ -22,7 +22,7 @@ while(True):
                 ou.plog("Exit")
                 break
         case 2:
-            ou.plog("Mode",i)
+            ou.plog("Mode",str(i))
             vrb = inp.Reader("Comp")
             ou.plog("Variables","("+ vrb[0]+")"+vrb[1]+"("+ vrb[2]+")")
             ou.plog("Result",str(cl.Calc(vrb)))
@@ -31,14 +31,14 @@ while(True):
                 ou.plog("Exit")
                 break
         case 3:
-            ou.plog("Mode",i)
+            ou.plog("Mode",str(i))
             ou.viewlog()
             Iface.Intface("End")
             if(int(inp.Variable()) == 2): 
                 ou.plog("Exit")
                 break
         case 4:
-            ou.plog("Mode",i)
+            ou.plog("Mode",str(i))
             print('Запущен выход из программы')
             break
         case _: print("Нет данных")
